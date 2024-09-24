ffb5d44ac3027a5e48f62ca1c3c8fad0a20e0e20
Paulo Tejada <paulotejada.123a@gmail.com>
2024-09-24 08:38:56 -0500

    create client

--- src/app/api/client.js
+++ src/app/api/client.js
@@ -15,4 +15,30 @@ export class Client {
       throw error;
     }
   }
+
+  async createClient() {
+    try {
+      const url = `${ENV.API_URL}${ENV.ENDPOINTS.CLIENTS}`;
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: {
+            ...data,
+          },
+        }),
+      });
+
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
